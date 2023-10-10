8a8af941c61073e27a99c6b6779bd08fb9c1cebd
ajeyy20 <147115719+ajeyy20@users.noreply.github.com>
2023-10-10 20:22:20 +0800

    Add files via upload

---
+++ act4.js
@@ -0,0 +1,14 @@
+let fruit = "banana";
+switch (fruit){
+  case "banana":
+    console.log("banana is good");
+    break;
+  case "orange":
+    consol.log("im not a fan of orange");
+    break;
+  case "apple":
+    console.log("How you like them apple?");
+    break;
+    default:
+    console.log("i have never heard of that fruit");
+    }
---
+++ act6.js
@@ -0,0 +1,7 @@
+let name = "AJeyy";
+let i = 10;
+while(i < 110)
+{
+    console.log(name, i);
+    i++;
+}
