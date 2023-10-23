0af5e86ff3793c5a58f7ad3bfd92ccb50d94837b
ajeyy20 <147115719+ajeyy20@users.noreply.github.com>
2023-10-23 20:03:43 +0800

    Add files via upload

---
+++ act7.js
@@ -0,0 +1,10 @@
+let password = "p@ssw0rd@2023";
+
+if(password.length >= 8 || password.includes("p@ssword"))
+{
+console.log("valid password")
+}
+else
+{
+console.log("invalid password")
+}
---
+++ act8.js
@@ -0,0 +1,28 @@
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. console.log the all the singers in that array.
+
+const favSingers = ["Chito Miranda", "Gloc 9","Andrew E."];
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+
+// 3. Create array name (favNumbers) & store 4 fav Numbers. console.log all in favNumbers array.
+var favNumbers = ['07', '20', '2021', '0720'];
+
+console.log(favNumbers[0]);
+console.log(favNumbers[1]);
+console.log(favNumbers[2]);
+console.log(favNumbers[3]);
+
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// 5. Now Access each item in that array by using [] notation. favSingers[0],  favSingers[1],  favSingers[2],  favNumbers [1] and lastly mixedArr[3]
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
